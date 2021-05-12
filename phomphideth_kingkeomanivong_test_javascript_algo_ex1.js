208f40e19bb87795eb23603c1581e863be9a0311
pkingkeo <83578015+pkingkeo@users.noreply.github.com>
2021-05-12 15:39:01 +0200

    Update phomphideth_kingkeomanivong_test_javascript_algo_ex1.js

--- phomphideth_kingkeomanivong_test_javascript_algo_ex1.js
+++ phomphideth_kingkeomanivong_test_javascript_algo_ex1.js
@@ -1,19 +1,20 @@
 let toVerify = [[], [], [], [], [], [], [], [], []];
 
+let toVerify = [[], [], [], [], [], [], [], [], []];
 function readTable(tableSource, tableTarget) {
-    if (tableSource.length === 9)
-        for (let i = 0; i < 9; i++) {
-            for (let j = 0; j < 9; j++) {
+    //if (tableSource.length === 9) {
+        for (let i = 0; i < tableSource.length ; i++) {
+            for (let j = 0; j < 9 ; j++) {
                 tableTarget[i][j] = tableSource[i].charAt(j * 2);
             }
         }
+    //}
 }
-
 function affiche(table) {
     let myTableHTML = ''; // '<table id="sudoku-table">';
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < table.length; i++) {
         myTableHTML += '<tr>'
-        for (let j = 0; j < 9; j++) {
+        for (let j = 0; j < table.length; j++) {
             myTableHTML += '<td>'+table[i][j]+'</td>';
         }
         myTableHTML += '</tr>'
