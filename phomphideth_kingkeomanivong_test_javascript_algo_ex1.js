4e515f003f219155b3861534a7ce561f27c7eabe
pkingkeo <83578015+pkingkeo@users.noreply.github.com>
2021-05-12 18:45:54 +0200

    Update phomphideth_kingkeomanivong_test_javascript_algo_ex1.js

--- phomphideth_kingkeomanivong_test_javascript_algo_ex1.js
+++ phomphideth_kingkeomanivong_test_javascript_algo_ex1.js
@@ -1,19 +1,20 @@
 let toVerify = [[], [], [], [], [], [], [], [], []];
 
 function readTable(tableSource, tableTarget) {
-    //if (tableSource.length === 9) {
-        for (let i = 0; i < tableSource.length ; i++) {
-            for (let j = 0; j < 9 ; j++) {
-                tableTarget[i][j] = tableSource[i].charAt(j * 2);
-            }
+    //console.log('readTable:tableSource.length='+tableSource.length )
+
+    for (let i = 0; i < tableSource.length; i++) {
+        for (let j = 0; j < tableTarget.length; j++) {
+            tableTarget[i][j] = tableSource[i].charAt(j * 2);
         }
-    //}
+    }
 }
+
 function affiche(table,tabId) {
-    let myTableHTML = ''; // '<table id="sudoku-table">';
+    let myTableHTML = '';
     for (let i = 0; i < table.length; i++) {
         myTableHTML += '<tr>'
-        for (let j = 0; j < table.length; j++) {
+        for (let j = 0; j < table[i].length; j++) {
             myTableHTML += '<td>'+table[i][j]+'</td>';
         }
         myTableHTML += '</tr>'
@@ -22,6 +23,7 @@ function affiche(table,tabId) {
     elt.innerHTML = myTableHTML;
 }
 
+
 // Lecture de la table array_number dans toVerify
-readTable(array_number,toVerify);
-affiche(toVerify,'sudoku-table');
+readTable(array_number, toVerify);
+affiche(toVerify, 'sudoku-table');
