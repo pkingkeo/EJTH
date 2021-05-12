3d748b2aea912567abb696c4c054074bdfd8bb8a
pkingkeo <83578015+pkingkeo@users.noreply.github.com>
2021-05-12 14:43:31 +0200

    Update phomphideth_kingkeomanivong_test_javascript_algo_ex2.js

--- phomphideth_kingkeomanivong_test_javascript_algo_ex2.js
+++ phomphideth_kingkeomanivong_test_javascript_algo_ex2.js
@@ -1,9 +1,9 @@
 function isDigit9Table(table) {
-    let listDigit9 = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let listDigit9 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    if ((table.length === 9)) {
-        for (let i = 0; i < 9; i++) {
-            for (let j = 0; j < 9; j++) {
+    if ((table.length === 3)) {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
 
                 if (listDigit9.indexOf(table[i][j]) < 0) {
                     return false;
@@ -18,4 +18,3 @@ function isDigit9Table(table) {
     }
     return true;
 }
-
