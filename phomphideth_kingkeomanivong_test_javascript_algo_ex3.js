4a6b7c140930f8138f41a8cc94a8d7af2da40447
pkingkeo <83578015+pkingkeo@users.noreply.github.com>
2021-05-13 02:14:00 +0200

    Update phomphideth_kingkeomanivong_test_javascript_algo_ex3.js

--- phomphideth_kingkeomanivong_test_javascript_algo_ex3.js
+++ phomphideth_kingkeomanivong_test_javascript_algo_ex3.js
@@ -1,27 +1,68 @@
-// Liste erreur par lignes
-function listLineError(table) {
+// check erreurs par lignes
+function checkLineError(table) {
 
     for (let i = 0; i < table.length; i++) {
         let testLine = table[i];
         let matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-        console.log('Testing L:' + i);
+
         readLineTable(testLine, matrix);
         let res = isUniqDigit9Table(matrix);
 
         if (!res) {
             let id = 'table-error-line' + i;
-            afficheTableLine(matrix, i, id);
+            afficheErrorTable(matrix, 'Line', i, id);
+            numberOfError++;
         }
     }
 }
 
+// check erreurs par colonnes
+function checkColError(table) {
 
-function afficheTableLine(tableLine, numLine, idTableHTML) {
-    let myTableHTML = '<tr><td>Line ' + numLine + ' incorrect:</td>';
-    for (let i = 0; i < tableLine.length; i++) {
+    for (let i = 0; i < table.length; i++) {
 
-        for (let j = 0; j < tableLine[i].length; j++) {
-            myTableHTML += '<td>' + tableLine[i][j] + '</td>';
+        let matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+
+        readColTable(table, i, matrix);
+        console.log(matrix);
+        let res = isUniqDigit9Table(matrix);
+
+        if (!res) {
+            let id = 'table-error-column' + i;
+            afficheErrorTable(matrix, 'Column', i, id);
+            numberOfError++;
+        }
+    }
+}
+
+// check erreurs par regions
+function checkRegionError(table) {
+
+    for (let i = 0; i < table.length; i++) {
+
+        let matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+
+        console.log('Testing R:' + i);
+        readRegionTable(table, i, matrix);
+
+        let res = isUniqDigit9Table(matrix);
+
+        if (!res) {
+            let id = 'table-error-region' + i;
+            afficheErrorTable(matrix, 'Region', i, id);
+            numberOfError++;
+        }
+    }
+}
+
+// Affichage Erreur Lignes/Col/Region
+function afficheErrorTable(table, typeError, num, idTableHTML) {
+    num++;
+    let myTableHTML = '<tr><td>' + typeError + ' ' + num + ' incorrect:</td>';
+    for (let i = 0; i < table.length; i++) {
+
+        for (let j = 0; j < table[i].length; j++) {
+            myTableHTML += '<td>' + table[i][j] + '</td>';
         }
 
     }
@@ -30,6 +71,7 @@ function afficheTableLine(tableLine, numLine, idTableHTML) {
     elt.innerHTML = myTableHTML;
 }
 
+// Lecture tables
 function readLineTable(tableLineSource, tableTarget) {
     console.log('readTableLine:tableLineSource.length=' + tableLineSource.length);
     let l = 0;
@@ -41,4 +83,88 @@ function readLineTable(tableLineSource, tableTarget) {
     }
 }
 
-listLineError(array_number);
+function readColTable(tableSource, colNumber, tableTarget) {
+
+    let l = 0;
+    let c = 0;
+    for (let i = 0; i < 9; i++) {
+        tableTarget[l][c++] = parseInt(tableSource[i][colNumber]);
+        if (c > 2) { c = 0; l++; }
+    }
+}
+
+function readRegionTable(tableSource, regionNumber, tableTarget) {
+
+    let l = 0;
+    let c = 0;
+    let startLine;
+    let startColumn;
+
+    switch (regionNumber) {
+        case 0:
+            startLine = 0;
+            startColumn = 0;
+            break;
+
+        case 1:
+            startLine = 0;
+            startColumn = 3;
+            break;
+
+        case 2:
+            startLine = 0;
+            startColumn = 6;
+            break;
+
+        case 3:
+            startLine = 3;
+            startColumn = 0;
+            break;
+
+        case 4:
+            startLine = 3;
+            startColumn = 3;
+            break;
+
+        case 5:
+            startLine = 3;
+            startColumn = 6;
+            break;
+
+        case 6:
+            startLine = 6;
+            startColumn = 0;
+            break;
+
+        case 7:
+            startLine = 6;
+            startColumn = 3;
+            break;
+
+        case 8:
+            startLine = 6;
+            startColumn = 6;
+            break;
+    }
+    console.log('startLine:'+startLine);
+    console.log('startColumn:'+startColumn);
+    for (let i = startLine; i < (startLine + 3); i++) {
+        for (let j = startColumn; j < (startColumn + 3); j++) {
+            tableTarget[l][c++] = parseInt(tableSource[i][j]);
+            if (c > 2) { c = 0; l++; }
+        }
+    }
+}
+
+let numberOfError = 0;
+checkLineError(array_number);
+checkColError(toVerify);
+checkRegionError(toVerify);
+
+let resultHTML = document.getElementById('check-result');
+if (numberOfError > 0) {
+    resultHTML.innerHTML = '<a></a>There are error(s):';
+} else {
+    resultHTML.innerHTML = '<a>The table is correct.</a>';
+}
+
